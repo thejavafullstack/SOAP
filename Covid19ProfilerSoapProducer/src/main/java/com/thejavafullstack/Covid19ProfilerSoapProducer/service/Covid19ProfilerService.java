92f8801efac98c499579e69c69637db4360245f4
WajidParray <Wajid.Parray@gmail.com>
2020-12-27 18:50:26 +0200

    Improved code with jaxb implementation

--- Covid19ProfilerSoapProducer/src/main/java/com/thejavafullstack/Covid19ProfilerSoapProducer/service/Covid19ProfilerService.java
+++ Covid19ProfilerSoapProducer/src/main/java/com/thejavafullstack/Covid19ProfilerSoapProducer/service/Covid19ProfilerService.java
@@ -3,33 +3,51 @@ package com.thejavafullstack.Covid19ProfilerSoapProducer.service;
 import com.thejavafullstack.Covid19ProfilerSoapProducer.model.Covid19CountryProfile;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import java.util.List;
 
-@WebService
+@WebService(name="Covid19Profiler",
+        targetNamespace="http://www.thejavafullstack.com")
 public interface Covid19ProfilerService {
 
-    @WebMethod
+    @WebMethod(action="getAllCovid19CountryProfiles", operationName="getAllCovid19CountryProfiles")
+    @WebResult(name="Covid19CountryProfiles")
     public List<Covid19CountryProfile> getAllCovid19CountryProfiles();
 
-    @WebMethod
-    public Covid19CountryProfile getCovid19CountryProfile(String countryName);
+    
+    @WebMethod(action="getCovid19CountryProfile", operationName="getCovid19CountryProfile")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile getCovid19CountryProfile(@WebParam(name = "countryName") String countryName);
 
-    @WebMethod
-    public Covid19CountryProfile updateCovid19CountryProfile(Covid19CountryProfile request);
+    
+    @WebMethod(action="updateCovid19CountryProfile", operationName="updateCovid19CountryProfile")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile updateCovid19CountryProfile(@WebParam(name = "covid19CountryProfile") Covid19CountryProfile covid19CountryProfile);
 
-    @WebMethod
-    public Covid19CountryProfile addCovid19CountryProfile( Covid19CountryProfile request);
+    
+    @WebMethod(action="addCovid19CountryProfile", operationName="addCovid19CountryProfile")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile addCovid19CountryProfile(@WebParam(name = "covid19CountryProfile") Covid19CountryProfile covid19CountryProfile);
 
-    @WebMethod
-    public Covid19CountryProfile removeCovid19CountryProfile( String countryName );
+    
+    @WebMethod(action="removeCovid19CountryProfile", operationName="removeCovid19CountryProfile")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile removeCovid19CountryProfile(@WebParam(name = "countryName") String countryName);
 
-    @WebMethod
-    public Covid19CountryProfile updateNumberOfCases( int numberOfCases  ,  String countryName);
+    
+    @WebMethod(action="updateNumberOfCases", operationName="updateNumberOfCases")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile updateNumberOfCases(@WebParam(name = "numberOfCases") int numberOfCases, @WebParam(name = "countryName") String countryName);
 
-    @WebMethod
-    public Covid19CountryProfile updateNumberOfRecoveries( int numberOfRecoveries  ,  String countryName);
+    
+    @WebMethod(action="updateNumberOfRecoveries", operationName="updateNumberOfRecoveries")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile updateNumberOfRecoveries(@WebParam(name = "numberOfRecoveries") int numberOfRecoveries, @WebParam(name = "countryName") String countryName);
 
-    @WebMethod
-    public Covid19CountryProfile updateNumberOfDeaths( int numberOfDeaths  ,  String countryName);
+    
+    @WebMethod(action="updateNumberOfDeaths", operationName="updateNumberOfDeaths")
+    @WebResult(name="Covid19CountryProfile")
+    public Covid19CountryProfile updateNumberOfDeaths(@WebParam(name = "numberOfDeaths") int numberOfDeaths, @WebParam(name = "countryName")  String countryName);
 }
--- Covid19ProfilerSoapProducer/src/main/java/com/thejavafullstack/Covid19ProfilerSoapProducer/service/Covid19ProfilerServiceImpl.java
+++ Covid19ProfilerSoapProducer/src/main/java/com/thejavafullstack/Covid19ProfilerSoapProducer/service/Covid19ProfilerServiceImpl.java
@@ -2,23 +2,25 @@ package com.thejavafullstack.Covid19ProfilerSoapProducer.service;
 
 import com.thejavafullstack.Covid19ProfilerSoapProducer.model.Covid19CountryProfile;
 import com.thejavafullstack.Covid19ProfilerSoapProducer.dao.Covid19CountryProfilerRepository;
-
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import java.util.List;
 
 @WebService(name="Covid19Profiler",
-        endpointInterface = "com.thejavafullstack.Covid19ProfilerSoapProducer.service.Covid19ProfilerService",
-        targetNamespace="https://www.thejavafullstack.com"
-)
+        endpointInterface="com.thejavafullstack.Covid19ProfilerSoapProducer.service.Covid19ProfilerService",
+        portName="Covid19ProfilerPort",
+        serviceName="Covid19ProfilerService",
+        targetNamespace="https://www.thejavafullstack.com")
 public class Covid19ProfilerServiceImpl implements Covid19ProfilerService {
 
     //@Autowired
     private Covid19CountryProfilerRepository covid19CountryProfilerRepository = new Covid19CountryProfilerRepository();
 
     @Override
-    @WebMethod
+    @WebMethod(action="getAllCovid19CountryProfiles", operationName="getAllCovid19CountryProfiles")
+    @WebResult(name="Covid19CountryProfiles")
     public List<Covid19CountryProfile> getAllCovid19CountryProfiles() {
         return covid19CountryProfilerRepository.getAllCovid19CountryProfiles();
     }
@@ -26,49 +28,49 @@ public class Covid19ProfilerServiceImpl implements Covid19ProfilerService {
     @Override
     @WebMethod(action="getCovid19CountryProfile", operationName="getCovid19CountryProfile")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile getCovid19CountryProfile(String countryName) {
+    public Covid19CountryProfile getCovid19CountryProfile(@WebParam(name = "countryName") String countryName) {
         return covid19CountryProfilerRepository.findCovid19CountryProfile(countryName);
     }
 
     @Override
     @WebMethod(action="updateCovid19CountryProfile", operationName="updateCovid19CountryProfile")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile updateCovid19CountryProfile(Covid19CountryProfile request) {
-        return covid19CountryProfilerRepository.updateCovid19CountryProfile(request);
+    public Covid19CountryProfile updateCovid19CountryProfile(@WebParam(name = "covid19CountryProfile") Covid19CountryProfile covid19CountryProfile) {
+        return covid19CountryProfilerRepository.updateCovid19CountryProfile(covid19CountryProfile);
     }
 
     @Override
     @WebMethod(action="addCovid19CountryProfile", operationName="addCovid19CountryProfile")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile addCovid19CountryProfile(Covid19CountryProfile request) {
-        return covid19CountryProfilerRepository.addCovid19CountryProfile(request);
+    public Covid19CountryProfile addCovid19CountryProfile(@WebParam(name = "covid19CountryProfile") Covid19CountryProfile covid19CountryProfile) {
+        return covid19CountryProfilerRepository.addCovid19CountryProfile(covid19CountryProfile);
     }
 
     @Override
     @WebMethod(action="removeCovid19CountryProfile", operationName="removeCovid19CountryProfile")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile removeCovid19CountryProfile(String countryName) {
+    public Covid19CountryProfile removeCovid19CountryProfile(@WebParam(name = "countryName") String countryName) {
         return covid19CountryProfilerRepository.removeCovid19CountryProfile(countryName);
     }
 
     @Override
     @WebMethod(action="updateNumberOfCases", operationName="updateNumberOfCases")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile updateNumberOfCases(int numberOfCases, String countryName) {
+    public Covid19CountryProfile updateNumberOfCases(@WebParam(name = "numberOfCases") int numberOfCases,@WebParam(name = "countryName")  String countryName) {
         return covid19CountryProfilerRepository.updateNumberOfCases(numberOfCases,countryName);
     }
 
     @Override
     @WebMethod(action="updateNumberOfRecoveries", operationName="updateNumberOfRecoveries")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile updateNumberOfRecoveries(int numberOfRecoveries, String countryName) {
+    public Covid19CountryProfile updateNumberOfRecoveries(@WebParam(name = "numberOfRecoveries") int numberOfRecoveries, @WebParam(name = "countryName") String countryName) {
         return covid19CountryProfilerRepository.updateNumberOfRecoveries(numberOfRecoveries,countryName);
     }
 
     @Override
     @WebMethod(action="updateNumberOfDeaths", operationName="updateNumberOfDeaths")
     @WebResult(name="Covid19CountryProfile")
-    public Covid19CountryProfile updateNumberOfDeaths(int numberOfDeaths, String countryName) {
+    public Covid19CountryProfile updateNumberOfDeaths(@WebParam(name = "numberOfDeaths") int numberOfDeaths,@WebParam(name = "countryName")  String countryName) {
         return covid19CountryProfilerRepository.updateNumberOfDeaths(numberOfDeaths,countryName);
     }
 }
